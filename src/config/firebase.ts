05c0825b090315245e785806726f563d2e25243c
HiroshiOyamada <h5y1m141@gmail.com>
2020-04-20 18:22:14 +0900

    Jest実行時にFirebaseのProject情報などを参照できるようにconfigをexportする形に変更

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,7 +2,7 @@ import * as firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/firestore'
 
-const config = {
+export const config = {
   apiKey: process.env.REACT_APP_FIRESTORE_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_Domain,
   databaseURL: process.env.REACT_APP_DATABASE_URL,
